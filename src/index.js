b49856ca9f48291a8236a0e261c893a44bb66b27
Kachisa D.N <kachdekan@gmail.com>
2021-07-08 17:55:21 +0200

    adding onMouser Event

--- src/index.js
+++ src/index.js
@@ -66,7 +66,12 @@ const AltBook = ({ imgsrc, imgalt, title, author }) => {
   };
 
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={imgsrc} alt={imgalt} />
       <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
