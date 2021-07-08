82b9fcd7b5722ae6eb86aecf34394fd5f1c3c20c
Kachisa D.N <kachdekan@gmail.com>
2021-07-08 14:14:47 +0200

    the first book

--- src/index.js
+++ src/index.js
@@ -4,7 +4,41 @@ import ReactDom from "react-dom";
 // Nested Components. React Tools
 
 function BookList() {
-  return <section>This is a booklist</section>;
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+      alt="i love you to the moon"
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>I Love You to the Moon and Back</h1>;
+};
+
+const Author = () => {
+  return <h4>Amalia Hepworth</h4>;
+};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
