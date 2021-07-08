6b9876d7a50fa87095dfe87a12fd42a21a2c98cb
Kachisa D.N <kachdekan@gmail.com>
2021-07-08 13:37:35 +0200

    JSX Rules :)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// JSX Rules
+// return single element
+// div / section / article / react-fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Greeting() {
+  return (
+    <div>
+      <h3>Hello People</h3>
+      <ul>
+        <li>
+          <a href="/">hello world</a>
+        </li>
+        <input type="text" name="" id="" />
+        <img src="" alt="" />
+      </ul>
+    </div>
+  );
+}
+
+ReactDom.render(<Greeting />, document.getElementById("root"));
