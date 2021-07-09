9e7cd518f312efa380ef57e4f7cf11b6f6c2780c
Kachisa D.N <kachdekan@gmail.com>
2021-07-09 16:24:18 +0200

    finalized Bday reminder + remove person

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
-import Setup from "./tutorial/1-useState/setup/5-useState-counter";
-
+import React, { useState } from "react";
+import data from "./projects/01-bday-reminder/setup/data";
+import List from "./projects/01-bday-reminder/setup/List";
 function App() {
+  const [people, setPeople] = useState(data);
+  const removeBday = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+    // console.log("Removed " + id);
+  };
   return (
-    <div className="container">
-      <Setup />
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} removeBday={removeBday} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
   );
 }
 
--- src/projects/01-bday-reminder/setup/List.js
+++ src/projects/01-bday-reminder/setup/List.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+const List = ({ people, removeBday }) => {
   return (
-    <>
-      <h2>list component</h2>
-    </>
+    <React.Fragment>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+            <button onClick={() => removeBday(id)}>Remove</button>
+          </article>
+        );
+      })}
+    </React.Fragment>
   );
 };
 
