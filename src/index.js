72181a7e51c233aee7fa3ddfb5eacbf7ca055014
Kachisa D.N <kachdekan@gmail.com>
2021-07-09 12:29:24 +0200

    Setting up the advanced tutorial

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-//Import and Export
-import "./index.css";
-
-import { books } from "./books";
-import Book from "./Book";
-
-function BookList() {
-  return (
-    <section className="booklist">
-      {books.map((book) => {
-        // const { imgsrc, imgalt, title, author } = book;
-        return <Book key={book.id} {...book}></Book>;
-      })}
-    </section>
-  );
-}
-
-ReactDom.render(<BookList />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
