1350c5c59b4938d2844f6709bbe56cf38924d4e2
Kachisa D.N <kachdekan@gmail.com>
2021-07-10 13:53:31 +0200

    finallized project 4. fixed icon issue

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React, { useState } from "react";
 import data from "./projects/04-accordion/setup/data";
 import SingleQuestion from "./projects/04-accordion/setup/Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
