fd1c2e9582fcde0fc512b498dc3fdaeb9a53a5f2
Kachisa D.N <kachdekan@gmail.com>
2021-07-10 10:32:32 +0200

    setting up project 2 (our tours)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import React, { useState } from "react";
-import data from "./projects/01-bday-reminder/setup/data";
-import List from "./projects/01-bday-reminder/setup/List";
+import React, { useState, useEffect } from "react";
+import Loading from "./projects/02-our-tours/setup/Loading";
+import Tours from "./projects/02-our-tours/setup/Tours";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  const [people, setPeople] = useState(data);
-  const removeBday = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
-    // console.log("Removed " + id);
-  };
-  return (
-    <main>
-      <section className="container">
-        <h3>{people.length} birthdays today</h3>
-        <List people={people} removeBday={removeBday} />
-        <button onClick={() => setPeople([])}>clear all</button>
-      </section>
-    </main>
-  );
+  return <h2>Tours Project Setup</h2>;
 }
 
 export default App;
