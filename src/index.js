f33b36a2ce5eb71de11a1250c506a4a6992b6fb3
Kachisa D.N <kachdekan@gmail.com>
2021-07-11 10:56:25 +0200

    finallized slider project.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./projects/06-tabs/setup/index.css";
+import "./projects/07-slider/setup/index.css";
 import App from "./App";
 
 ReactDOM.render(
