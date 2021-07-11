79dcdb697b77c60972dac566f6303c8ff505ec9e
Kachisa D.N <kachdekan@gmail.com>
2021-07-11 09:27:23 +0200

    setting up project 7 (slider project)

--- src/App.js
+++ src/App.js
@@ -1,69 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { FaAngleDoubleRight } from "react-icons/fa";
-const url = "https://course-api.com/react-tabs-project";
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [jobs, setJobs] = useState([]);
-  const [value, setValue] = useState(0);
-
-  const fetchJobs = async () => {
-    const response = await fetch(url);
-    const newJobs = await response.json();
-    setJobs(newJobs);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchJobs();
-  }, []);
-
-  if (loading) {
-    return (
-      <section>
-        <h1>loading...</h1>
-      </section>
-    );
-  }
-  const { company, dates, duties, title } = jobs[value]; // wait for fetch
-  return (
-    <section className="section ">
-      <div className="title">
-        <h2>experience</h2>
-        <div className="underline"></div>
-      </div>
-      <div className="jobs-center">
-        <div className="btn-container">
-          {jobs.map((item, index) => {
-            return (
-              <button
-                key={index}
-                onClick={() => setValue(index)}
-                className={`job-btn ${index === value && "active-btn"}`}
-              >
-                {" "}
-                {/*key as item id*/}
-                {item.company}
-              </button>
-            );
-          })}
-        </div>
-
-        <article className="job-info">
-          <h3>{title}</h3>
-          <h4>{company}</h4>
-          <p className="job-date">{dates}</p>
-          {duties.map((duty, index) => {
-            return (
-              <div key={index} className="job-desc">
-                <FaAngleDoubleRight className="job-icon" />
-                <p>{duty}</p>
-              </div>
-            );
-          })}
-        </article>
-      </div>
-    </section>
-  );
+  return <h2>slider project setup</h2>;
 }
 
 export default App;
