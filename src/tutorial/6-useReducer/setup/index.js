f3d4f361e0684359c6161172f4a1a95546c422df
Kachisa D.N <kachdekan@gmail.com>
2021-07-12 17:36:43 +0200

    refactor reducer to indipendent component

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,38 +1,10 @@
 import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../../data";
-// reducer function
-const reducer = (state, action) => {
-  if (action.type === "ADD_ITEM") {
-    const newPeople = [...state.people, action.payload];
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: "item is added",
-    };
-  }
-  if (action.type === "NO_VALUE") {
-    return { ...state, isModalOpen: true, modalContent: "please enter value" };
-  }
-  if (action.type === "CLOSE_MODAL") {
-    return { ...state, isModalOpen: false };
-  }
-  if (action.type === "REMOVE_ITEM") {
-    const newPeople = state.people.filter(
-      (person) => person.id !== action.payload
-    );
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: "item removed",
-    };
-  }
-  throw new Error("no matching action type");
-};
+import { reducer } from "./reducer";
+
 const defaultState = {
-  people: [],
+  people: data,
   isModalOpen: false,
   modalContent: "hey there",
 };
