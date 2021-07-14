061d4177f548a58a2d6193d850d37d9472bc6a7a
Kachisa D.N <kachdekan@gmail.com>
2021-07-14 20:34:05 +0200

    setting up next two projects

--- src/App.js
+++ src/App.js
@@ -1,103 +1,11 @@
-import React, { useState, useEffect } from "react";
-import List from "./projects/10-grocery-bud/setup/List";
-import Alert from "./projects/10-grocery-bud/setup/Alert";
-
-const getLocalStorage = () => {
-  let list = localStorage.getItem("list");
-  if (list) {
-    return JSON.parse(localStorage.getItem("list"));
-  } else {
-    return [];
-  }
-};
+import React from "react";
+import Navbar from "./projects/11-navbar/setup/Navbar";
 
 function App() {
-  const [name, setName] = useState("");
-  const [list, setList] = useState(getLocalStorage);
-  const [isEditing, setIsEditing] = useState(false);
-  const [editId, setEditId] = useState(null);
-  const [alert, setAlert] = useState({
-    show: false,
-    msg: "",
-    type: "",
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!name) {
-      // setAlert({ show: true, msg: "please enter value", type: "danger" });
-      showAlert(true, "danger", "please enter value");
-    } else if (name && isEditing) {
-      setList(
-        list.map((item) => {
-          if (item.id === editId) {
-            return { ...item, title: name };
-          }
-        })
-      );
-      setName("");
-      setEditId(null);
-      setIsEditing(false);
-      showAlert(true, "success", "edit item");
-    } else {
-      showAlert(true, "success", "item added to list");
-      const newItem = { id: new Date().getTime().toString(), title: name };
-      setList([...list, newItem]);
-      setName("");
-    }
-  };
-
-  const showAlert = (show = false, type = "", msg = "") => {
-    setAlert({ show, type, msg });
-  };
-
-  const clearList = () => {
-    showAlert(true, "danger", "empty list");
-    setList([]);
-  };
-
-  const removeItem = (id) => {
-    showAlert(true, "danger", "item removed");
-    setList(list.filter((item) => item.id !== id));
-  };
-
-  const editItem = (id) => {
-    const specItem = list.find((item) => item.id === id);
-    setIsEditing(true);
-    setEditId(id);
-    setName(specItem.title);
-  };
-
-  useEffect(() => {
-    localStorage.setItem("list", JSON.stringify(list));
-  }, [list]);
   return (
-    <section className="section-center">
-      <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
-        <h3>grocery bud</h3>
-        <div className="form-control">
-          <input
-            type="text"
-            className="grocery"
-            placeholder="e.g. eggs"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <button type="submit" className="submit-btn">
-            {isEditing ? "edit" : "submit"}
-          </button>
-        </div>
-      </form>
-      {list.length > 0 && (
-        <div className="grocery-container">
-          <List items={list} removeItem={removeItem} editItem={editItem} />
-          <button type="submit" className="clear-btn" onClick={clearList}>
-            clear items
-          </button>
-        </div>
-      )}
-    </section>
+    <React.Fragment>
+      <h2>navbar project setup</h2>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./projects/10-grocery-bud/setup/index.css";
+import "./projects/11-navbar/setup/index.css";
 import App from "./App";
 
 ReactDOM.render(
