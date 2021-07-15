f02eac4000b9def82df09220c58371bbe3d37f63
Kachisa D.N <kachdekan@gmail.com>
2021-07-15 18:10:15 +0200

    Finallized the cocktail projects

--- src/projects/15-cocktails/setup/components/SearchForm.js
+++ src/projects/15-cocktails/setup/components/SearchForm.js
@@ -1,12 +1,37 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React, { useRef, useEffect } from "react";
+import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  return (
-    <div>
-      <h2>search form component</h2>
-    </div>
-  )
-}
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = useRef("");
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
 
-export default SearchForm
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  return (
+    <section className="section search">
+      <form action="" className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your drink</label>
+          <input
+            type="text"
+            name=""
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
--- src/projects/15-cocktails/setup/pages/About.js
+++ src/projects/15-cocktails/setup/pages/About.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente cum
+        pariatur odit eum incidunt. Eveniet maxime illum deserunt optio iusto.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, saepe?
+        Culpa rem delectus eius laborum amet unde accusamus! Blanditiis
+        provident quasi eaque veritatis minus. Labore pariatur cumque obcaecati
+        sunt placeat ratione hic rem? Blanditiis ut debitis cumque facere rem
+        asperiores soluta ea quae quia veritatis, consequuntur, aliquid
+        repellendus ratione minus.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
